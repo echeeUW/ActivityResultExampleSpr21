05339a6f207cae682651dbdf58013b528063fd69
Eric Chee <eric.chee@hulu.com>
2021-04-15 15:57:52 -0700

    Added result callback to load tweets into a list from another activity

--- app/src/main/java/edu/uw/echee/activityresultexample/CreateTweetActivity.kt
+++ app/src/main/java/edu/uw/echee/activityresultexample/CreateTweetActivity.kt
@@ -1,8 +1,12 @@
 package edu.uw.echee.activityresultexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.uw.echee.activityresultexample.databinding.ActivityCreateTweetBinding
+
+const val USERNAME_KEY = "USERNAME_KEY"
+const val TWEET_MSG_KEY = "TWEET_MSG_KEY"
 
 class CreateTweetActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateTweetBinding
@@ -11,6 +15,18 @@ class CreateTweetActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateTweetBinding.inflate(layoutInflater).apply { setContentView(root) }
         with(binding) {
+
+            btnTweet.setOnClickListener {
+                val username = etUsername.text.toString()
+                val tweetMsg = etTweetMsg.text.toString()
+
+                setResult(RESULT_OK, Intent().apply {
+                    putExtra(USERNAME_KEY, username)
+                    putExtra(TWEET_MSG_KEY, tweetMsg)
+                })
+                finish()
+            }
+
         }
     }
 }
--- app/src/main/java/edu/uw/echee/activityresultexample/MainActivity.kt
+++ app/src/main/java/edu/uw/echee/activityresultexample/MainActivity.kt
@@ -3,8 +3,12 @@ package edu.uw.echee.activityresultexample
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import edu.uw.echee.activityresultexample.databinding.ActivityMainBinding
 import edu.uw.echee.activityresultexample.recyclerview.TweetsAdapter
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,7 +20,23 @@ class MainActivity : AppCompatActivity() {
         with(binding) {
 
             btnCompose.setOnClickListener {
-                startActivity(Intent(this@MainActivity, CreateTweetActivity::class.java))
+                requestForData.launch(Intent(this@MainActivity, CreateTweetActivity::class.java))
+            }
+        }
+    }
+
+    private val requestForData = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // We have been given data!
+
+            val extraData = result.data?.extras
+            if (extraData != null) {
+                // Pull the data out of the intent that was sent back to us
+                val username = extraData.getString(USERNAME_KEY)
+                val tweetMsg = extraData.getString(TWEET_MSG_KEY)
+                Log.i(TAG, "@$username : $tweetMsg")
+
+                addTweet(tweetMsg)
             }
 
         }
@@ -24,12 +44,9 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
-
-
     // region List of Tweets
     private lateinit var tweetsAdapter: TweetsAdapter
+
     private fun initListOfTweets() = with(binding) {
         tweetsAdapter = TweetsAdapter(listOf())
         rvTweets.adapter = tweetsAdapter
